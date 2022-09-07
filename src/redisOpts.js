d32b0248df993b3570f479e2a418570ab392baee
Patrick Hagemeister <github@hagemeister.cc>
2022-09-07 14:30:47 +0200

    Configurate redis db number

--- src/redisOpts.js
+++ src/redisOpts.js
@@ -1,10 +1,14 @@
 // @format
 var url = process.env.REDIS_URL || 'redis://127.0.0.1:6379/';
+var redisDatabase = process.env.REDIS_DB || 0;
+if (typeof(redisDatabase) === 'string') {
+  redisDatabase = parseInt(redisDatabase);
+}
 var redisOpts = require('redis-url').parse(url);
 
 module.exports = {
   host: redisOpts.hostname,
   port: redisOpts.port,
   password: redisOpts.password,
-  options: {db: redisOpts.database || 0},
+  options: {db: redisDatabase},
 };
